ac8dcc1a1dfc3c2f751168f48faf6a9ac885d75c
Valery Burau <vburau9@tygrys.robotyka.ia.pw.edu.pl>
2018-10-26 11:53:37 +0200

    New version

--- myRoboTest.py
+++ myRoboTest.py
@@ -3,63 +3,74 @@ from ev3dev2.motor import SpeedPercent
 from ev3dev2.sensor.lego import ColorSensor
 from threading import Thread
 from time import sleep
-import keyboard
+from ev3dev2.sound import Sound
+#import keyboard
 
 lm = LargeMotor('outA')
-lm2 = LargeMotor('outB')
+lm2 = LargeMotor('outD')
 cs = ColorSensor("in1")
 cs2 = ColorSensor("in2")
-cs_black = False
-cs2_black = False
-i = 0
+#global cs_black 
+#global cs2_black
+#global i
+s = Sound()
+s2 = Sound()
+cs_black = {'0' : False} 
+cs2_black = {'0' : False} 
+#cs2_black = False
+i = {'0' : 0} 
 
 # to measure reflected light intensity. In this mode the sensor will return a value between 0 and 100
 cs.mode = 'COL-REFLECT'
 cs2.mode = 'COL-REFLECT'
 
 def notBlack(cs,cs2):
-    while True:
-        #cs2.reflected_light_intensity < 40
-        #depends on a value of i we'll know which of two sensors was the last one(who detected black color)
-        if(cs.value < 40):
-            cs_black == True
-            if(cs_black and cs2_black == False):
-                i == 1
+    
+    while(True):
+        if(cs.value() < 20):
+            cs_black['0'] = True
+            if(cs_black['0'] and cs2_black['0'] == False):
+                i['0'] = 1
         else:
-            cs_black == False
+            cs_black['0'] = False
 
-        if (cs.value < 40):
-            cs2_black == True
-            if (cs2_black and cs_black == False):
-                i == 2
+        if (cs2.value() < 20):
+            cs2_black['0'] = True
+            if (cs2_black['0'] and cs_black['0'] == False):
+                i['0'] = 2
         else:
-            cs2_black == False
+            cs2_black['0'] = False
 
 t = Thread(target=notBlack, args=(cs,cs2))
 t.start()
-
-while(keyboard.is_pressed('ENTER') != True):
-    while(cs_black or cs2_black):
-        lm.run_to_rel_pos(position_sp=4, speed_sp=SpeedPercent(20), stop_action="hold")
-        lm2.run_to_rel_pos(position_sp=4, speed_sp=SpeedPercent(20), stop_action="hold")
-        sleep(1)
-
-    lm.run_to_rel_pos(position_sp=4, speed_sp=SpeedPercent(20), stop_action="hold")
-    lm2.run_to_rel_pos(position_sp=4, speed_sp=SpeedPercent(20), stop_action="hold")
-
-    if(i==1):
-        while(i==1):
-            if(cs_black or cs2_black):
-                break
-            else:
+#keyboard.is_pressed('ENTER') != 
+while(True):
+    sleep(1)
+    while(cs_black['0'] or cs2_black['0']):
+       lm.run_forever(speed_sp=SpeedPercent(200), stop_action="hold")
+       lm2.run_forever(speed_sp=SpeedPercent(200), stop_action="hold")
+       # lm.run_to_rel_pos(position_sp=50, speed_sp=SpeedPercent(40), stop_action="hold")
+       # lm2.run_to_rel_pos(position_sp=50, speed_sp=SpeedPercent(40), stop_action="hold")
+        
+    if(i['0']==1):
+        if(cs_black['0'] or cs2_black['0']):
+            pass
+        else:
+            while(cs_black['0']==False and cs2_black['0']==False):
+                
                 lm.run_to_rel_pos(position_sp=0, stop_action="hold")
-                lm2.run_to_rel_pos(position_sp=2, speed_sp=SpeedPercent(20), stop_action="hold")
-                break
-    elif(i==2):
-        while(i==2):
-            if (cs_black or cs2_black):
-                break
-            else:
-                lm.run_to_rel_pos(position_sp=2, speed_sp=SpeedPercent(20), stop_action="hold")
+                lm2.run_to_rel_pos(position_sp=10, speed_sp=SpeedPercent(100), stop_action="hold")
+                #sleep(5)
+                #s.beep()
+                
+            pass    
+    elif(i['0']==2):
+        if (cs_black['0'] or cs2_black['0']):
+            pass
+        else:
+            while(cs_black['0']==False and cs2_black['0']==False):
+                lm.run_to_rel_pos(position_sp=10, speed_sp=SpeedPercent(100), stop_action="hold")
                 lm2.run_to_rel_pos(position_sp=0, stop_action="hold")
-                break
+                #sleep(5)
+                #s2.speak('Hello, I am Robot')
+            pass
