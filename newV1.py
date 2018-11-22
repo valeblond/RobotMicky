17dbdc16a27aa0725102f5f7a5b8dce16b8cc8fc
Valery Burau <vburau9@tygrys.robotyka.ia.pw.edu.pl>
2018-11-22 15:57:03 +0100

    gedgee

--- newV1.py
+++ newV1.py
@@ -5,8 +5,9 @@ from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sound import Sound
 from threading import Thread
 from time import sleep
+from ev3dev.ev3 import *
 
-lm = LargeMotor('outB')
+lm = LargeMotor('outA')
 lm2 = LargeMotor('outD')
 cs = ColorSensor("in1")
 cs2 = ColorSensor("in2")
@@ -21,51 +22,65 @@ cs2.mode = 'COL-REFLECT'
 
 
 def stan(cs, cs2):
-    while (True):
+    while True:
         # dla zwyklej jazdy
-        if (cs.value() <= 33 and cs.value() > 25): #jechac prosto
+        if (cs.value() <= 33 and cs.value() > 20): #jechac prosto
             z['0'] = 1
-        elif (cs.value() < 25): #troche skrecic w lewo(za duzo czarnego)
+            print("1" + "\n" + "\n")
+        elif (cs.value() <= 20): #troche skrecic w lewo(za duzo czarnego)
+            print("2" + "\n" + "\n")
             z['0'] = 2
         elif (cs.value() > 33): #troche skrecic w prawo(za malo czarnego)
+            print("3" + "\n" + "\n")
             z['0'] = 3
 
         # dla powrotow i skrzyzowan
-        if (cs.value() < 25 and cs2.value() < 25):  # jechac prosto (na skrzyzowaniu)
+        if (cs.value() < 20 and cs2.value() < 20):  # jechac prosto (na skrzyzowaniu)
+            print("4" + "\n" + "\n")
             q['0'] = 1
-        elif (cs.value() < 25 and cs2.value() >= 30):  # skret w lewo
-            q['0'] = 2
-        elif (z['0'] == 1 and cs2.value() < 25):  # skret w prawo
-            q['0'] = 3
-        elif (True):
+#        elif (cs.value() < 15 and cs2.value() >= 37):  # skret w lewo
+ #           q['0'] = 2
+  #      elif (z['0'] == 1 and cs2.value() < 15):  # skret w prawo
+   #         q['0'] = 3
+    #    elif (cs.value() > 33  and cs2.value() < 25):  # skret w prawo
+     #       q['0'] = 4
+        elif True:
             q['0'] = 0
+            print("5" + "\n" + "\n")
 
 t = Thread(target=stan, args=(cs, cs2))
 t.start()
 
 sleep(1)
-while (True):
+while True:
     while ((z['0'] == 1 or q['0'] == 1) and q['0'] != 2 and q['0'] != 3 ):
-        lm.run_forever(speed_sp=200, stop_action="hold")
-        lm2.run_forever(speed_sp=200, stop_action="hold")
+    	lm.run_forever(speed_sp=200, stop_action="hold")
+    	lm2.run_forever(speed_sp=200, stop_action="hold")
+    print("6" + "\n")
 
     if (z['0'] == 2 and q['0'] == 0):
         while (z['0'] != 1 or q['0'] != 1):
-            lm.run_to_rel_pos(position_sp=0, stop_action="hold")
-            lm2.run_to_rel_pos(position_sp=1, speed_sp=600, stop_action="hold")
+            lm.run_forever(speed_sp=0, stop_action="hold")
+            lm2.run_forever(speed_sp=100, stop_action="hold")
+        lm.run_to_rel_pos(position_sp=0, speed_sp=0, stop_action="hold")
+        lm2.run_to_rel_pos(position_sp=0, speed_sp=0, stop_action="hold")       
+        print("7" + "\n" + "\n")
         continue
     elif (z['0'] == 3 and q['0'] == 0):
         while (z['0'] != 1):
-            lm.run_to_rel_pos(position_sp=1, speed_sp=600, stop_action="hold")
-            lm2.run_to_rel_pos(position_sp=0, stop_action="hold")
+            lm2.run_forever(speed_sp=0, stop_action="hold")
+            lm.run_forever(speed_sp=100, stop_action="hold")
+        lm.run_to_rel_pos(position_sp=0, speed_sp=0, stop_action="hold")
+        lm2.run_to_rel_pos(position_sp=0, speed_sp=0, stop_action="hold")       
+        print("8" + "\n" + "\n")
         continue
-    elif (q['0'] == 3):
-        while (cs2.value() < 25):
-            lm.run_to_rel_pos(position_sp=1, speed_sp=600, stop_action="hold")
-            lm2.run_to_rel_pos(position_sp=0, stop_action="hold")
-        continue
-    elif (q['0'] == 2):
-        while (cs.value() < 25):
-            lm.run_to_rel_pos(position_sp=0, stop_action="hold")
-            lm2.run_to_rel_pos(position_sp=1, speed_sp=600, stop_action="hold")
-        continue
+#    elif (q['0'] == 3):
+  #      while (cs2.value() < 20):
+   #         lm2.run_forever(speed_sp=0, stop_action="hold")
+    #        lm.run_forever(speed_sp=100, stop_action="hold")
+     #   continue
+ #   elif (q['0'] == 2):
+  #      while (cs.value() < 20):
+   #         lm.run_forever(speed_sp=0, stop_action="hold")
+    #        lm2.run_forever(speed_sp=100, stop_action="hold")
+     #   continue
--- newV2.py
+++ newV2.py
@@ -29,20 +29,11 @@ deriv = {'0': 0}
 lastError = {'0': 0}
 u = {'0': 0}
 
-def pdi():
-    error['0'] = target['0'] - cs.value()
-    integ['0'] += error['0']
-    deriv['0'] = error['0'] - lastError['0']
-    # u zero:     on target,  drive forward
-    # u positive: too bright, turn right
-    # u negative: too dark,   turn left
-    u['0'] = Kp['0']*error['0'] + Ki['0']*integ['0'] + Kd['0']*deriv['0']
-    lastError['0'] = error['0']
 
 def stan(cs, cs2):
     while (True):
         # dla zwyklej jazdy
-        if (cs.value() <= 33 and cs.value() > 25): #jechac prosto
+        if (cs.value() <= 33 and cs.value() > 22): #jechac prosto
             z['0'] = 1
         elif (cs.value() < 25): #troche skrecic w lewo(za duzo czarnego)
             z['0'] = 2
@@ -52,9 +43,9 @@ def stan(cs, cs2):
         # dla powrotow i skrzyzowan
         if (cs.value() < 25 and cs2.value() < 25):  # jechac prosto (na skrzyzowaniu)
             q['0'] = 1
-        elif (cs.value() < 25 and cs2.value() >= 30):  # skret w lewo
+        elif (cs.value() < 15 and cs2.value() >= 37):  # skret w lewo
             q['0'] = 2
-        elif (z['0'] == 1 and cs2.value() < 25):  # skret w prawo
+        elif (z['0'] == 1 and cs2.value() < 15):  # skret w prawo
             q['0'] = 3
         elif (True):
             q['0'] = 0
@@ -64,8 +55,16 @@ t.start()
 
 sleep(1)
 while (True):
-    while ((z['0'] == 1 or q['0'] == 1) and q['0'] != 2 and q['0'] != 3):
-        pdi()
+    while ((z['0'] == 1 or q['0'] == 1) and q['0'] != 2 and q['0'] != 3 ):
+        error['0'] = target['0'] - int(cs.value())
+        integ['0'] += error['0']
+        deriv['0'] = error['0'] - lastError['0']
+        # u zero:     on target,  drive forward
+        # u positive: too bright, turn right
+        # u negative: too dark,   turn left
+        u['0'] = Kp['0']*error['0'] + Ki['0']*integ['0'] + Kd['0']*deriv['0']
+        lastError['0'] = error['0']
+        
         if(u['0'] >= 0):
             lm.run_forever(speed_sp=200+u['0'], stop_action="hold")
             lm2.run_forever(speed_sp=200-u['0'], stop_action="hold")
@@ -76,20 +75,20 @@ while (True):
     if (z['0'] == 2 and q['0'] == 0):
         while (z['0'] != 1 or q['0'] != 1):
             lm.run_to_rel_pos(position_sp=0, stop_action="hold")
-            lm2.run_to_rel_pos(position_sp=1, speed_sp=600, stop_action="hold")
+            lm2.run_to_rel_pos(position_sp=1, speed_sp=400, stop_action="hold")
         continue
     elif (z['0'] == 3 and q['0'] == 0):
         while (z['0'] != 1):
-            lm.run_to_rel_pos(position_sp=1, speed_sp=600, stop_action="hold")
+            lm.run_to_rel_pos(position_sp=1, speed_sp=400, stop_action="hold")
             lm2.run_to_rel_pos(position_sp=0, stop_action="hold")
         continue
     elif (q['0'] == 3):
-        while (cs2.value() < 25):
-            lm.run_to_rel_pos(position_sp=1, speed_sp=600, stop_action="hold")
+        while (cs2.value() < 20):
+            lm.run_to_rel_pos(position_sp=1, speed_sp=400, stop_action="hold")
             lm2.run_to_rel_pos(position_sp=0, stop_action="hold")
         continue
     elif (q['0'] == 2):
-        while (cs.value() < 25):
+        while (cs.value() < 20):
             lm.run_to_rel_pos(position_sp=0, stop_action="hold")
-            lm2.run_to_rel_pos(position_sp=1, speed_sp=600, stop_action="hold")
+            lm2.run_to_rel_pos(position_sp=1, speed_sp=400, stop_action="hold")
         continue
