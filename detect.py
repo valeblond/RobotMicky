f8a89df14cc9cf2b542827f1fc62201065b9469f
Valery989 <valera_9796@mail.ru>
2018-11-23 00:27:04 +0100

    lastChance

---
+++ detect.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import LargeMotor
+from ev3dev2.motor import SpeedPercent
+from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sound import Sound
+from threading import Thread
+from time import sleep
+from ev3dev.ev3 import *
+
+lm = LargeMotor('outA')
+lm2 = LargeMotor('outD')
+cs = ColorSensor("in1")
+cs2 = ColorSensor("in2")
+ir = InfraredSensor("in3")
+
+# to measure reflected light intensity. In this mode the sensor will return a value between 0 and 100
+cs.mode = 'COL-COLOR'
+cs2.mode = 'COL-COLOR'
+# Put the infrared sensor into proximity mode.
+ir.mode = 'IR-PROX'
+
+q = {'0': 0} # For detecting what color he has found
+k = {'0': 0} # For detecting if we are close to some other object
+i = {'0': 0} # For detecting if we've already made a round for needed color
+
+def distance(ir):
+    while True:
+        if(ir.value() > 15):
+            k['0'] == 1     # okay
+        else:
+            k['0'] == 2     # need turn
+
+def notBlack(cs, cs2):
+    while (True):
+        if ((cs.value() == 5 or cs2.value() == 5) and q['0'] == 0): # Find red color
+            q['0'] == 1
+        if ((cs.value() == 2 or cs2.value() == 2) and q['0'] == 1): # Find blue color
+            q['0'] == 2
+        if ((cs.value() == 3 or cs2.value() == 3) and q['0'] == 2): # Find green color
+            q['0'] == 3
+        if ((cs.value() == 4 or cs2.value() == 4) and q['0'] == 3): # Find yellow color
+            q['0'] == 4
+
+t = Thread(target=notBlack, args=(cs, cs2))
+d = Thread(target=distance, args=(ir))
+t.start()
+d.start()
+
+sleep(1)
+while (True):
+    while (k['0'] == 1 and q['0'] == 0):       # Looking for red square
+        lm.run_forever(speed_sp=150, stop_action="hold")
+        lm2.run_forever(speed_sp=150, stop_action="hold")
+    if (k['0'] == 1 and q['0'] == 1 and i['0'] == 0):          # When he's found red color
+        i['0'] = 1
+        lm.run_to_rel_pos(position_sp=0, speed_sp=0, stop_action="hold")
+        lm2.run_to_rel_pos(position_sp=30, speed_sp=600, stop_action="hold")
+        sleep(3)
+        Sound.beep()
+    while (k['0'] == 1 and q['0'] == 1):       # Looking for blue square
+        lm.run_forever(speed_sp=150, stop_action="hold")
+        lm2.run_forever(speed_sp=150, stop_action="hold")
+    if (k['0'] == 1 and q['0'] == 2 and i['0'] == 1):          # When he's found blue color
+        i['0'] = 2
+        lm.run_to_rel_pos(position_sp=0, speed_sp=0, stop_action="hold")
+        lm2.run_to_rel_pos(position_sp=30, speed_sp=600, stop_action="hold")
+        sleep(3)
+        Sound.beep()
+    while (k['0'] == 1 and q['0'] == 2):       # Looking for green square
+        lm.run_forever(speed_sp=150, stop_action="hold")
+        lm2.run_forever(speed_sp=150, stop_action="hold")
+    if (k['0'] == 1 and q['0'] == 3 and i['0'] == 2):          # When he's found green color
+        i['0'] = 3
+        lm.run_to_rel_pos(position_sp=0, speed_sp=0, stop_action="hold")
+        lm2.run_to_rel_pos(position_sp=30, speed_sp=600, stop_action="hold")
+        sleep(3)
+        Sound.beep()
+    while (k['0'] == 1 and q['0'] == 3):       # Looking for yellow square
+        lm.run_forever(speed_sp=150, stop_action="hold")
+        lm2.run_forever(speed_sp=150, stop_action="hold")
+    if (k['0'] == 1 and q['0'] == 4 and i['0'] == 3):          # When he's found yellow color
+        i['0'] = 4
+        lm.run_to_rel_pos(position_sp=0, speed_sp=0, stop_action="hold")
+        lm2.run_to_rel_pos(position_sp=30, speed_sp=600, stop_action="hold")
+        sleep(3)
+        Sound.beep()
+    if(k['0'] == 2):
+        lm.run_to_rel_pos(position_sp=0, speed_sp=0, stop_action="hold")
+        lm2.run_to_rel_pos(position_sp=30, speed_sp=700, stop_action="hold")
+        sleep(1)
+
+
+
+
---
+++ myRoboTest5.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import LargeMotor
+from ev3dev2.motor import SpeedPercent
+from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sound import Sound
+from threading import Thread
+from time import sleep
+from ev3dev.ev3 import *
+
+lm = LargeMotor('outB')
+lm2 = LargeMotor('outD')
+cs = ColorSensor("in1")
+cs2 = ColorSensor("in2")
+cs_black = {'0': False}
+cs2_black = {'0': False}
+i = {'0': 0}
+k = {'0': 0}
+
+# to measure reflected light intensity. In this mode the sensor will return a value between 0 and 100
+cs.mode = 'COL-REFLECT'
+cs2.mode = 'COL-REFLECT'
+
+
+def notBlack(cs, cs2):
+    while (True):
+        if (cs.value() < 30):
+            cs_black['0'] = True
+            if (cs_black['0'] and cs2_black['0'] == False):
+                i['0'] = 1
+        else:
+            cs_black['0'] = False
+
+        if (cs2.value() < 30):
+            cs2_black['0'] = True
+            if (cs2_black['0'] and cs_black['0'] == False):
+                i['0'] = 2
+        else:
+            cs2_black['0'] = False
+
+        if(cs.value() < 22 or cs2.value() < 22):
+            if (cs.value() > 10 and cs.value() < 30 and cs2.value() < 10): # turn right
+                k['0'] == 1
+
+            if (cs.value() < 10 and cs2.value() > 10 and cs2.value() < 30): # turn left
+                k['0'] == 1
+
+            if (cs.value() < 10 and cs2.value() < 10):
+                if(cs.value() > cs2.value()):
+                    k['0'] == 3             # turn right
+                else:
+                    k['0'] == 4             # turn left
+        else:
+            k['0'] == 0
+
+t = Thread(target=notBlack, args=(cs, cs2))
+t.start()
+
+sleep(1)
+while (True):
+    while ((cs_black['0'] or cs2_black['0']) and k['0'] == 0):
+        sleep(0.4)
+        lm.run_forever(speed_sp=150, stop_action="hold")
+        lm2.run_forever(speed_sp=150, stop_action="hold")
+
+    lm.stop(stop_action="hold")
+    lm2.stop(stop_action="hold")
+
+    if (i['0'] == 1 and k['0'] == 0):
+        while (cs_black['0'] == False and cs2_black['0'] == False):
+            lm.run_to_rel_pos(position_sp=0, stop_action="hold")
+            lm2.run_to_rel_pos(position_sp=7, speed_sp=200, stop_action="hold")
+        lm.run_to_rel_pos(position_sp=0, stop_action="hold")
+        lm2.run_to_rel_pos(position_sp=30, speed_sp=500, stop_action="hold")
+    elif (i['0'] == 2 and k['0'] == 0):
+        while (cs_black['0'] == False and cs2_black['0'] == False):
+            lm.run_to_rel_pos(position_sp=7, speed_sp=200, stop_action="hold")
+            lm2.run_to_rel_pos(position_sp=0, stop_action="hold")
+        lm.run_to_rel_pos(position_sp=30, speed_sp=500, stop_action="hold")
+        lm2.run_to_rel_pos(position_sp=0, stop_action="hold")
+
+    if(k['0'] == 1 or k['0'] == 3):     # turn right
+        while(k['0'] != 0):
+            lm.run_to_rel_pos(position_sp=7, speed_sp=200, stop_action="hold")
+            lm2.run_to_rel_pos(position_sp=0, stop_action="hold")
+        lm.run_to_rel_pos(position_sp=30, speed_sp=500, stop_action="hold")
+        lm2.run_to_rel_pos(position_sp=0, stop_action="hold")
+    elif(k['0'] == 2 or k['0'] == 4):   # turn left
+        while(k['0'] != 0):
+            lm.run_to_rel_pos(position_sp=0, stop_action="hold")
+            lm2.run_to_rel_pos(position_sp=7, speed_sp=200, stop_action="hold")
+        lm.run_to_rel_pos(position_sp=0, stop_action="hold")
+        lm2.run_to_rel_pos(position_sp=30, speed_sp=500, stop_action="hold")
+
